a3bba4530c649e85860fe6b660837cbe98bf75e3
Jackson Wilmot <wilmotj28@barker.college>
2025-01-14 15:53:51 +0700

    Changed socials to open a new tab

--- src/routes/SocialContacts.svelte
+++ src/routes/SocialContacts.svelte
@@ -12,27 +12,27 @@
 
 <div>
     <!-- svelte-ignore a11y_consider_explicit_label -->
-    <a href="https://www.facebook.com">
+    <a target="_blank" href="https://www.facebook.com">
         <i class="fa-brands fa-facebook"></i>
     </a>
 
     <!-- svelte-ignore a11y_consider_explicit_label -->
-    <a href="https://www.instagram.com">
+    <a target="_blank" href="https://www.instagram.com">
         <i class="fa-brands fa-instagram"></i>
     </a>
 
     <!-- svelte-ignore a11y_consider_explicit_label -->
-    <a href="https://www.youtube.com">
+    <a target="_blank" href="https://www.youtube.com">
         <i class="fa-brands fa-youtube"></i>
     </a>
 
     <!-- svelte-ignore a11y_consider_explicit_label -->
-    <a href="https://www.facebook.com">
+    <a target="_blank" href="https://www.facebook.com">
         <i class="fa-brands fa-discord"></i>
     </a>
 
     <!-- svelte-ignore a11y_consider_explicit_label -->
-    <a href="https://www.pintrest.com">
+    <a target="_blank" href="https://www.pintrest.com">
         <i class="fa-brands fa-pinterest"></i>
     </a>
 </div>
